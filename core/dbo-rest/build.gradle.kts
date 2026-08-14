ee82ce54de086a6661dae585819c2fd10828b55e
alanluuk <alanluuk@gmail.com>
2026-08-14 17:43:15 +0300

    Thin bundles compute their imports with bnd; fat embedders stay curated (#13)

--- core/dbo-rest/build.gradle.kts
+++ core/dbo-rest/build.gradle.kts
@@ -1,7 +1,10 @@
+plugins {
+    id("biz.aQute.bnd.builder")
+}
+
 // The FHIR HTTP surface (dbo#12): JDK HttpServer on virtual threads, ZERO new
-// dependencies (R2). Version-generic: serves any FhirStoreFacade. One server
-// instance = one tenant store; multi-tenant dispatch belongs to the routing
-// layer.
+// dependencies (R2). Version-generic: serves any FhirStoreFacade.
+// bnd computes Import-Package from bytecode (dbo#13).
 
 dependencies {
     api(project(":core:dbo-core"))
@@ -9,17 +12,11 @@ dependencies {
 }
 
 tasks.jar {
-    manifest {
-        attributes(
-            "Bundle-ManifestVersion" to "2",
+    bundle {
+        bnd(mapOf(
             "Bundle-SymbolicName" to "cloud.jengu.dbo.rest",
             "Bundle-Version" to "0.1.0",
-            "Export-Package" to "cloud.jengu.dbo.rest;version=\"0.1.0\"",
-            "Import-Package" to listOf(
-                "cloud.jengu.dbo.core.api;version=\"[0.1,1)\"",
-                "cloud.jengu.dbo.fhir.common;version=\"[0.1,1)\"",
-                "com.sun.net.httpserver",
-            ).joinToString(","),
-        )
+            "Export-Package" to "cloud.jengu.dbo.rest;version=0.1.0",
+        ))
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+pluginManagement {
+    plugins {
+        id("biz.aQute.bnd.builder") version "7.1.0"
+    }
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "dbo"
 
 include("core:dbo-core", "core:dbo-postgres", "core:dbo-fhir-common", "core:dbo-fhir-r4", "core:dbo-fhir-r5", "core:dbo-rest", "core:dbo-subscriptions", "core:dbo-terminology", "core:dbo-test-model", "core:harness")
