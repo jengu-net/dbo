2dd7a11f7fd68561a0ef0b44690bd01b42ca9021
alanluuk <alanluuk@gmail.com>
2026-08-14 17:37:06 +0300

    Production bundles boot in Felix and speak FHIR from inside (#13)

--- core/dbo-fhir-r4/build.gradle.kts
+++ core/dbo-fhir-r4/build.gradle.kts
@@ -4,13 +4,73 @@
 // the OSGi private-embedding packaging is the spike-proven pattern applied in
 // a later packaging task.
 
+val embedded: Configuration by configurations.creating
+configurations.implementation.get().extendsFrom(embedded)
+
 dependencies {
     api(project(":core:dbo-core"))
     api(project(":core:dbo-fhir-common"))
     api(project(":core:dbo-subscriptions"))
     api(project(":core:dbo-terminology"))
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:8.10.1")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-validation:8.10.1")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:8.10.1")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-caching-caffeine:8.10.1")
+    embedded("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:8.10.1")
+    embedded("ca.uhn.hapi.fhir:hapi-fhir-validation:8.10.1")
+    embedded("ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:8.10.1")
+    embedded("ca.uhn.hapi.fhir:hapi-fhir-caching-caffeine:8.10.1")
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    into("lib") { from(embedded) }
+    doFirst {
+        val libs = embedded.resolve().joinToString(",") { "lib/${it.name}" }
+        manifest {
+            attributes(
+                "Bundle-ManifestVersion" to "2",
+                "Bundle-SymbolicName" to "cloud.jengu.dbo.fhir.r4",
+                "Bundle-Version" to "0.1.0",
+                "Bundle-ClassPath" to ".,$libs",
+                "Export-Package" to "cloud.jengu.dbo.fhir.r4;version=\"0.1.0\"",
+                "Import-Package" to listOf(
+                    "cloud.jengu.dbo.core.api;version=\"[0.1,1)\"",
+                    "cloud.jengu.dbo.core.api.feed;version=\"[0.1,1)\"",
+                    "cloud.jengu.dbo.core;version=\"[0.1,1)\"",
+                    "cloud.jengu.dbo.fhir.common;version=\"[0.1,1)\"",
+                    "cloud.jengu.dbo.subscriptions;version=\"[0.1,1)\"",
+                    "cloud.jengu.dbo.terminology;version=\"[0.1,1)\"",
+                    "javax.naming;resolution:=optional",
+                    "javax.naming.spi;resolution:=optional",
+                    "javax.management;resolution:=optional",
+                    "javax.net;resolution:=optional",
+                    "javax.net.ssl;resolution:=optional",
+                    "javax.crypto;resolution:=optional",
+                    "javax.crypto.spec;resolution:=optional",
+                    "javax.security.auth;resolution:=optional",
+                    "javax.security.auth.callback;resolution:=optional",
+                    "javax.security.auth.x500;resolution:=optional",
+                    "javax.security.sasl;resolution:=optional",
+                    "javax.sql;resolution:=optional",
+                    "javax.xml.parsers;resolution:=optional",
+                    "javax.xml.stream;resolution:=optional",
+                    "javax.xml.stream.events;resolution:=optional",
+                    "javax.xml.stream.util;resolution:=optional",
+                    "javax.xml.transform;resolution:=optional",
+                    "javax.xml.transform.dom;resolution:=optional",
+                    "javax.xml.transform.sax;resolution:=optional",
+                    "javax.xml.transform.stax;resolution:=optional",
+                    "javax.xml.transform.stream;resolution:=optional",
+                    "javax.xml.validation;resolution:=optional",
+                    "javax.xml.namespace;resolution:=optional",
+                    "javax.xml.datatype;resolution:=optional",
+                    "javax.xml.xpath;resolution:=optional",
+                    "org.xml.sax;resolution:=optional",
+                    "org.xml.sax.helpers;resolution:=optional",
+                    "org.xml.sax.ext;resolution:=optional",
+                    "org.w3c.dom;resolution:=optional",
+                    "org.w3c.dom.ls;resolution:=optional",
+                    "org.w3c.dom.events;resolution:=optional",
+                    "org.ietf.jgss;resolution:=optional",
+                ).joinToString(","),
+            )
+        }
+    }
 }
