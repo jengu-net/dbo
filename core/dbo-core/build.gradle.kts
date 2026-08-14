1b4294e41957603463d039d81e7feabcb5a667a6
alanluuk <alanluuk@gmail.com>
2026-08-14 17:48:17 +0300

    Bundle versions come from the project version

--- core/dbo-core/build.gradle.kts
+++ core/dbo-core/build.gradle.kts
@@ -14,7 +14,6 @@ tasks.jar {
     bundle {
         bnd(mapOf(
             "Bundle-SymbolicName" to "cloud.jengu.dbo.core",
-            "Bundle-Version" to "0.1.0",
             "Export-Package" to "cloud.jengu.dbo.core.api.*;version=0.1.0,cloud.jengu.dbo.core;version=0.1.0",
         ))
     }
